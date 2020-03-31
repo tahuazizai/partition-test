76179ccda461b26a1f41e3b0a12a5eeeb20e3074
wb-hj706015 <wb-hj706015@alibaba-inc.com>
2020-03-31 15:53:46 +0800

    分布式框架

--- partition-test/partition-dao/src/main/java/com/group/partitionDao/dao/SysMenuMapper.java
+++ partition-test/partition-dao/src/main/java/com/group/partitionDao/dao/SysMenuMapper.java
@@ -1,6 +1,6 @@
 package com.group.partitionDao.dao;
 
-import com.group.partitionDao.dao.baseDao.MyBaseMapper;
+import com.group.partitionDao.baseDao.MyBaseMapper;
 import com.group.partitioninterface.entity.SysMenu;
 
 public interface SysMenuMapper extends MyBaseMapper<SysMenu> {
